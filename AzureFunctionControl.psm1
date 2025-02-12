85167c76c57ca252a805053e20186eed47c0c9c5
Matt <macavall@microsoft.com>
2025-02-12 12:14:51 -0600

    Module

---
+++ AzureFunctionControl.psm1
@@ -0,0 +1,68 @@
+﻿# AzureFunctionControl.psm1
+
+function Stop-AzFunctionApp {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $ResourceGroupName,
+
+        [Parameter(Mandatory = $true)]
+        [string] $FunctionAppName
+    )
+
+    # Authenticate and get an access token
+    $accessToken = (az account get-access-token --query accessToken --output tsv)
+    $apiVersion = "2024-04-01"
+
+    # Get the Function App resource
+    $functionApp = Get-AzWebApp -ResourceGroupName $ResourceGroupName -Name $FunctionAppName
+    $resourceId = $functionApp.Id
+
+    # Construct the URL
+    $managementUrl = "https://management.azure.com" + $resourceId + "?api-version=$apiVersion"
+
+    # Stop the Function App
+    $stopBody = @{
+        properties = @{
+            state = "Stopped"
+            scmSiteAlsoStopped = $true
+        }
+    } | ConvertTo-Json -Depth 3
+
+    Invoke-RestMethod -Uri $managementUrl -Method PATCH -Body $stopBody -ContentType "application/json" -Headers @{ Authorization = "Bearer $accessToken" }
+    Write-Host "Function App '$FunctionAppName' in Resource Group '$ResourceGroupName' has been stopped."
+}
+
+function Start-AzFunctionApp {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $ResourceGroupName,
+
+        [Parameter(Mandatory = $true)]
+        [string] $FunctionAppName
+    )
+
+    # Authenticate and get an access token
+    $accessToken = (az account get-access-token --query accessToken --output tsv)
+    $apiVersion = "2024-04-01"
+
+    # Get the Function App resource
+    $functionApp = Get-AzWebApp -ResourceGroupName $ResourceGroupName -Name $FunctionAppName
+    $resourceId = $functionApp.Id
+
+    # Construct the URL
+    $managementUrl = "https://management.azure.com" + $resourceId + "?api-version=$apiVersion"
+
+    # Start the Function App
+    $startBody = @{
+        properties = @{
+            state = "Running"
+            scmSiteAlsoStopped = $false
+        }
+    } | ConvertTo-Json -Depth 3
+
+    Invoke-RestMethod -Uri $managementUrl -Method PATCH -Body $startBody -ContentType "application/json" -Headers @{ Authorization = "Bearer $accessToken" }
+    Write-Host "Function App '$FunctionAppName' in Resource Group '$ResourceGroupName' has been started."
+}
+
+# Export module functions
+Export-ModuleMember -Function Stop-AzFunctionApp, Start-AzFunctionApp
